cecf28dc60d94bc440b76f5b1106bad5627047a7
renanvbaAlmeida <ralmeida@sjcc.com.br>
2023-08-22 14:46:12 -0300

    [FEAT] implementação de function

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const item = document.getElementById("item");
-const button = document.getElementById("button");
+const botao = document.getElementById("botao");
 const produtos = document.getElementById("Produtos");
+
+function add(){
+    const itemName = item.value.trim();
+    if (itemName != ""){
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `
+        ${itemName}
+        <button class="remove">Remover</button>
+        `;
+        produtos.appendChild(listItem);
+        item.value = "";
+
+        const removeButton = document.querySelectorAll(".remove");
+        removeButton.forEach(button =>{
+            button.addEventListener("click", () =>{
+                produtos.removeChild(button.parentNode);
+            });
+        });
+    }
+}
